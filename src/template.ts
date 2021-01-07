c91486b19faa95a23a253408f17f18d990c7a2f1
Andrey Mikheychik <a.mikheychik@gmail.com>
2021-01-06 22:12:34 -0500

    Update @angular-eslint to v1.0.0

--- src/template.ts
+++ src/template.ts
@@ -18,11 +18,22 @@ export = {
             files: ['*.component.html'],
             parser: '@angular-eslint/template-parser',
             rules: {
+                // Functionality
                 '@angular-eslint/template/accessibility-alt-text': 'error',
                 '@angular-eslint/template/accessibility-elements-content': 'error',
                 '@angular-eslint/template/accessibility-table-scope': 'error',
                 '@angular-eslint/template/accessibility-valid-aria': 'error',
                 '@angular-eslint/template/banana-in-box': 'warn',
+                '@angular-eslint/template/click-events-have-key-events': 'error',
+                // TODO: Does not report "mouseover" events
+                '@angular-eslint/template/mouse-events-have-key-events': 'error',
+                '@angular-eslint/template/no-any': 'error',
+                '@angular-eslint/template/no-autofocus': 'error',
+                '@angular-eslint/template/no-distracting-elements': 'error',
+                // TODO: Fails with "Cannot read property 'start' of undefined
+                '@angular-eslint/template/no-negated-async': 'off',
+                '@angular-eslint/template/no-positive-tabindex': 'error', // == accessibility-tabindex-no-positive
+                // Maintainability
                 '@angular-eslint/template/conditional-complexity': ['error', {
                     // Max complexity 2 allows *ngIf to have only two conditions.
                     //  Passes: *ngIf="isPublished && hasAccess"
@@ -32,17 +43,8 @@ export = {
                 }],
                 '@angular-eslint/template/cyclomatic-complexity': 'off',
                 '@angular-eslint/template/i18n': 'off',
-                '@angular-eslint/template/click-events-have-key-events': 'error',
-                // TODO: Does not report "mouseover" events
-                '@angular-eslint/template/mouse-events-have-key-events': 'error',
-                '@angular-eslint/template/no-any': 'error',
-                '@angular-eslint/template/no-autofocus': 'error',
                 // TODO: Fails with "Cannot read property 'start' of undefined
                 '@angular-eslint/template/no-call-expression': 'off',
-                '@angular-eslint/template/no-distracting-elements': 'error',
-                // TODO: Fails with "Cannot read property 'start' of undefined
-                '@angular-eslint/template/no-negated-async': 'off',
-                '@angular-eslint/template/no-positive-tabindex': 'error',
                 '@angular-eslint/template/use-track-by-function': 'error',
             },
         },
