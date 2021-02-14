9ca5dcd648caaaab967d12a9e1813003bb463527
Andrey Mikheychik <a.mikheychik@gmail.com>
2021-02-14 08:40:21 -0500

    Apply eslint-config overrides only to TypeScript files

--- src/community.ts
+++ src/community.ts
@@ -2,11 +2,13 @@ export = {
     extends: [
         './',
         '@perfective/eslint-config/community',
-        './rules/eslint-config',
     ],
     overrides: [
         {
             files: ['*.ts'],
+            extends: [
+                './rules/eslint-config',
+            ],
             rules: {
                 // Undesired behavior in functions like forkJoin
                 'rxjs/finnish': 'off',
