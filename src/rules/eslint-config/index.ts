aa3feff017a0bb33ad6fd1bf355f0b3f7a6c0b5c
Andrey Mikheychik <a.mikheychik@gmail.com>
2021-02-14 13:23:36 -0500

    Update @perfective/eslint-config to v0.12.0-beta
    * Remove @perfective/eslint-config-angular/community

--- src/rules/eslint-config/index.ts
+++ src/rules/eslint-config/index.ts
@@ -4,24 +4,25 @@ import { typescriptEslintNamingConvention } from './rules/typescript-eslint';
 
 export = {
     rules: {
+        // Conflicts with decorators
+        'new-cap': 'off',
         '@typescript-eslint/naming-convention': ['error', ...typescriptEslintNamingConvention()],
         // Legacy rules for TSLint and Codelyzer
         '@typescript-eslint/tslint/config': ['warn', typescriptEslintTslintConfig()],
         // The default, Angular-friendly, configuration
-        'rxjs/finnish': [
-            'error', {
-                functions: true,
-                methods: true,
-                names: {
-                    '^(canActivate|canActivateChild|canDeactivate|canLoad|intercept|resolve|validate)$': false,
-                },
-                parameters: true,
-                properties: true,
-                types: {
-                    '^EventEmitter$': false,
-                },
-                variables: true,
+        'rxjs/finnish': ['error', {
+            functions: true,
+            methods: true,
+            names: {
+                '^(canActivate|canActivateChild|canDeactivate|canLoad|intercept|resolve|validate)$': false,
             },
-        ],
+            parameters: true,
+            properties: true,
+            strict: false,
+            types: {
+                '^EventEmitter$': false,
+            },
+            variables: true,
+        }],
     },
 };
