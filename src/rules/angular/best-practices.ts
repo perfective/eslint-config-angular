1452febdb957495d999ec797a7ba54da7190dd3d
Andrey Mikheychik <a.mikheychik@gmail.com>
2022-04-04 02:23:41 -0500

    Update @angular-eslint to v13.2.0

--- src/rules/angular/best-practices.ts
+++ src/rules/angular/best-practices.ts
@@ -1,6 +1,9 @@
 export = {
     rules: {
         '@angular-eslint/no-empty-lifecycle-method': 'error',
+        '@angular-eslint/require-localize-metadata': ['error', {
+            requireDescription: false,
+        }],
         '@angular-eslint/sort-ngmodule-metadata-arrays': 'warn',
     },
 };
