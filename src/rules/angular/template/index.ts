2c34938a6fa371298fe2d8ad2b6afd73030b8d18
Andrey Mikheychik <a.mikheychik@gmail.com>
2022-12-28 06:24:19 -0600

    Group @angular-eslint/template plugin rules in one file
    Original split by functionality and maintanability was based on the Codelyzer groups. But the @angular-eslint plugin does not use groupings. So it is easier to maintain the rules once they are in the same file.
    
    Update documentation as well.

--- src/rules/angular/template/index.ts
+++ src/rules/angular/template/index.ts
@@ -2,14 +2,15 @@ export = {
     plugins: [
         '@angular-eslint/template',
     ],
-    extends: [
-        './functionality',
-        './maintainability',
-    ],
     rules: {
+        '@angular-eslint/template/accessibility-alt-text': 'error',
+        '@angular-eslint/template/accessibility-elements-content': 'error',
         '@angular-eslint/template/accessibility-interactive-supports-focus': 'error',
+        '@angular-eslint/template/accessibility-label-for': 'off',
         '@angular-eslint/template/accessibility-label-has-associated-control': 'error',
         '@angular-eslint/template/accessibility-role-has-required-aria': 'error',
+        '@angular-eslint/template/accessibility-table-scope': 'warn',
+        '@angular-eslint/template/accessibility-valid-aria': 'error',
         '@angular-eslint/template/attributes-order': ['warn', {
             alphabetical: false,
             order: [
@@ -21,12 +22,34 @@ export = {
                 'ATTRIBUTE_BINDING',
             ],
         }],
+        '@angular-eslint/template/banana-in-box': 'warn',
         '@angular-eslint/template/button-has-type': 'error',
+        '@angular-eslint/template/click-events-have-key-events': 'error',
+        '@angular-eslint/template/conditional-complexity': ['error', {
+            // Max complexity 2 allows *ngIf to have only two conditions.
+            //  Passes: *ngIf="isPublished && hasAccess"
+            //  Fails:  *ngIf="isPublished && hasAccess && isMobile"
+            // Use pipes to extract conditions logic
+            maxComplexity: 2,
+        }],
+        '@angular-eslint/template/cyclomatic-complexity': 'off',
         '@angular-eslint/template/eqeqeq': 'warn',
+        '@angular-eslint/template/i18n': 'off',
+        // TODO: Does not report "mouseover" events
+        '@angular-eslint/template/mouse-events-have-key-events': 'error',
+        '@angular-eslint/template/no-any': 'error',
+        '@angular-eslint/template/no-autofocus': 'warn',
+        // TODO: Fails with "Cannot read property 'start' of undefined
+        '@angular-eslint/template/no-call-expression': 'off',
+        '@angular-eslint/template/no-distracting-elements': 'warn',
         '@angular-eslint/template/no-duplicate-attributes': 'error',
         '@angular-eslint/template/no-inline-styles': ['error', {
             allowNgStyle: false,
             allowBindToStyle: false,
         }],
+        // TODO: Fails with "Cannot read property 'start' of undefined
+        '@angular-eslint/template/no-negated-async': 'off',
+        '@angular-eslint/template/no-positive-tabindex': 'error', // == accessibility-tabindex-no-positive
+        '@angular-eslint/template/use-track-by-function': 'error',
     },
 };
