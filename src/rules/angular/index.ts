9303ae50eeafa2ca4c005ac024c71beb92386dfb
Andrey Mikheychik <a.mikheychik@gmail.com>
2022-12-28 06:42:10 -0600

    Group @angular-eslint plugin rules in one file
    Original split by functionality and maintanability was based on the Codelyzer groups. But the @angular-eslint plugin does not use groupings. So it is easier to maintain the rules once they are in the same file.
    
    Update documentation as well.

--- src/rules/angular/index.ts
+++ src/rules/angular/index.ts
@@ -2,16 +2,63 @@ export = {
     plugins: [
         '@angular-eslint',
     ],
-    extends: [
-        './functionality',
-        './maintainability',
-        './style',
-    ],
     rules: {
+        '@angular-eslint/component-class-suffix': 'error',
+        '@angular-eslint/component-max-inline-declarations': ['error', {
+            animations: 0,
+            styles: 0,
+            template: 10,
+        }],
+        '@angular-eslint/component-selector': ['error', {
+            type: 'attribute',
+            prefix: 'pf',
+            style: 'camelCase',
+        }],
+        '@angular-eslint/contextual-decorator': 'error',
+        '@angular-eslint/contextual-lifecycle': 'error',
+        '@angular-eslint/directive-class-suffix': 'error',
+        // TODO: Does not support tag-specific selector
+        '@angular-eslint/directive-selector': ['error', {
+            type: 'attribute',
+            prefix: 'pf',
+            style: 'camelCase',
+        }],
+        '@angular-eslint/no-attribute-decorator': 'error',
+        '@angular-eslint/no-conflicting-lifecycle': 'error',
         '@angular-eslint/no-empty-lifecycle-method': 'error',
+        '@angular-eslint/no-forward-ref': 'error',
+        '@angular-eslint/no-host-metadata-property': ['error', {
+            allowStatic: false,
+        }],
+        '@angular-eslint/no-input-prefix': ['error', {
+            prefixes: ['can', 'is', 'on', 'should'],
+        }],
+        '@angular-eslint/no-input-rename': ['off', {
+            allowedNames: [],
+        }],
+        '@angular-eslint/no-inputs-metadata-property': 'error',
+        '@angular-eslint/no-lifecycle-call': 'error',
+        '@angular-eslint/no-output-native': 'error',
+        '@angular-eslint/no-output-on-prefix': 'error',
+        '@angular-eslint/no-output-rename': 'error',
+        '@angular-eslint/no-outputs-metadata-property': 'error',
+        '@angular-eslint/no-pipe-impure': 'error',
+        '@angular-eslint/no-queries-metadata-property': 'error',
+        '@angular-eslint/pipe-prefix': ['error', {
+            prefixes: ['pf'],
+        }],
+        '@angular-eslint/prefer-on-push-component-change-detection': 'error',
+        '@angular-eslint/prefer-output-readonly': 'error',
+        // TODO: Does not support outside directory paths (e.g. ../common.scss)
+        '@angular-eslint/relative-url-prefix': 'error',
         '@angular-eslint/require-localize-metadata': ['error', {
             requireDescription: false,
         }],
         '@angular-eslint/sort-ngmodule-metadata-arrays': 'warn',
+        '@angular-eslint/use-component-selector': 'error',
+        '@angular-eslint/use-component-view-encapsulation': 'error',
+        '@angular-eslint/use-injectable-provided-in': 'error',
+        '@angular-eslint/use-lifecycle-interface': 'error',
+        '@angular-eslint/use-pipe-transform-interface': 'warn',
     },
 };
