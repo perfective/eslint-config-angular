a19b342a22c77427ba6fab1f3a23089634f62d2e
Andrey Mikheychik <a.mikheychik@gmail.com>
2023-08-06 21:00:33 -0500

    Use @perfective/eslint-config-angular (self) as an ESLint config
    While there are no Angular files in the repository, it helps to prevent the most trivial bugs for now.
    
    - Disable the `@angular-eslint/require-localize-metadata` rule.

--- src/rules/angular/index.ts
+++ src/rules/angular/index.ts
@@ -9,7 +9,8 @@ export = {
     ],
     rules: {
         '@angular-eslint/no-empty-lifecycle-method': 'error',
-        '@angular-eslint/require-localize-metadata': ['error', {
+        // See https://github.com/angular-eslint/angular-eslint/pull/1469
+        '@angular-eslint/require-localize-metadata': ['off', {
             requireDescription: false,
         }],
         '@angular-eslint/sort-ngmodule-metadata-arrays': 'warn',
