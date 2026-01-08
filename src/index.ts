637ce40c2731d32b8de764091eb497e98002934d
Andrey Mikheychik <a.mikheychik@gmail.com>
2026-01-08 17:45:45 -0600

    Move `rules/angular` into `config/angular-eslint` directory
    - Align the directory structure in the `eslint-config-angular` with the `eslint-config`.
    - Move `rules/angular/template/angular-template-config.ts` into `config/angular-eslint` directory.

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { Linter } from 'eslint';
 
 import { perfectiveEslintConfig } from '@perfective/eslint-config';
 
-import { angularConfig } from './rules/angular/angular-config';
-import { angularTemplateConfig } from './rules/angular/template/angular-template-config';
+import { angularConfig } from './config/angular-eslint/angular-config';
+import { angularTemplateConfig } from './config/angular-eslint/angular-template-config';
 import { eslintConfig } from './rules/eslint-config/eslint-config';
 
 export function perfectiveEslintAngularConfig(configs: Linter.Config[] = []): Linter.Config[] {
