384545b5a90eb075539806db6a85d742be7e0ec2
Andrey Mikheychik <a.mikheychik@gmail.com>
2026-01-08 18:11:15 -0600

    Inline `eslintConfig()` into `perfectiveEslintAngularConfig()`

--- src/config/perfective-eslint-angular-config.ts
+++ src/config/perfective-eslint-angular-config.ts
@@ -1,16 +1,39 @@
 import { Linter } from 'eslint';
 
-import { perfectiveEslintConfig } from '@perfective/eslint-config';
+import { perfectiveEslintConfig, typescriptFiles } from '@perfective/eslint-config';
 
 import { angularConfig } from './angular-eslint/angular-config';
 import { angularTemplateConfig } from './angular-eslint/angular-template-config';
-import { eslintConfig } from './eslint-config/eslint-config';
+import { typescriptEslintNamingConvention } from './typescript-eslint/rules/typescript-eslint';
 
 export function perfectiveEslintAngularConfig(configs: Linter.Config[] = []): Linter.Config[] {
     return perfectiveEslintConfig([
         angularConfig,
         angularTemplateConfig,
-        eslintConfig,
+        // Override @perfective/eslint-config to support Angular.
+        {
+            files: typescriptFiles,
+            rules: {
+                // Conflicts with decorators
+                'new-cap': 'off',
+                '@typescript-eslint/naming-convention': ['error', ...typescriptEslintNamingConvention()],
+                // The default, Angular-friendly, configuration
+                'rxjs-x/finnish': ['error', {
+                    functions: true,
+                    methods: true,
+                    names: {
+                        '^(canActivate|canActivateChild|canDeactivate|canLoad|intercept|resolve|validate)$': false,
+                    },
+                    parameters: true,
+                    properties: true,
+                    strict: false,
+                    types: {
+                        '^EventEmitter$': false,
+                    },
+                    variables: true,
+                }],
+            },
+        },
         ...configs,
     ]);
 }
